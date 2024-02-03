b44e971713a01c4cab47aa001b6c690cf604e95c
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2024-02-03 18:20:20 +0500

    Added the main Cpp File

---
+++ MainProgram.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include"InteractionSystem.h"
+using namespace std;
+
+int main()
+{
+    // A Main Object to help our system Run 
+    InteractionSystem MySys;
+    MySys.MainMenue();
+}
